2d71c4b1c31fed76ec4285a3ecd00a5a6a04ab9e
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-08-07 20:46:46 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
+import { loadLayersModel, IOHandler } from '@tensorflow/tfjs-layers';
+
+import modelJson from './vgg16_model.json';
+
+const customIOHandler: IOHandler = {
+  async load() {
+    return {
+      modelTopology: modelJson.modelTopology,
+      format: modelJson.format,
+    };
+  },
+  async save() {
+    return {};
+  },
+};
 
 function App() {
-  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [isModelLoading, setIsModelLoading] = useState(true);
   const [model, setModel] = useState(null);
   const [imageURL, setImageURL] = useState(null);
   const [results, setResults] = useState([]);
@@ -15,9 +30,8 @@ function App() {
   const fileInputRef = useRef();
 
   const loadModel = async () => {
-    setIsModelLoading(true);
     try {
-      const loadedModel = await tf.loadLayersModel('./src/vgg16_model.json');
+      const loadedModel = await loadLayersModel(customIOHandler);
       setModel(loadedModel);
       setIsModelLoading(false);
     } catch (error) {
@@ -46,13 +60,13 @@ function App() {
       await img.decode();
 
       // Preprocess the image
-      const tensorImg = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat();
+      const tensorImg = tf.browser.fromPixels(img).resizeNearestNeighbor([240, 240]).toFloat();
       const offset = tf.scalar(127.5);
       const normalized = tensorImg.sub(offset).div(offset).expandDims();
 
       // Make the prediction
-      const prediction = await model.predict(normalized).data();
-      setResults(prediction);
+      const prediction = await model.predict(normalized).array();
+      setResults(prediction[0]);
 
       // Clean up the tensor
       tensorImg.dispose();
@@ -84,43 +98,49 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className='header'>Image Identification</h1>
-      <div className='inputHolder'>
-        <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
-        <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
-        <span className='or'>OR</span>
-        <input type="text" placeholder='Paster image URL' ref={textInputRef} onChange={handleOnChange} />
+      <h1 className="header">Image Identification</h1>
+      <div className="inputHolder">
+        <input type="file" accept="image/*" capture="camera" className="uploadInput" onChange={uploadImage} ref={fileInputRef} />
+        <button className="uploadImage" onClick={triggerUpload}>
+          Upload Image
+        </button>
+        <span className="or">OR</span>
+        <input type="text" placeholder="Paste image URL" ref={textInputRef} onChange={handleOnChange} />
       </div>
       <div className="mainWrapper">
         <div className="mainContent">
           <div className="imageHolder">
             {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} />}
           </div>
-          {results.length > 0 && <div className='resultsHolder'>
-            {results.map((result, index) => {
-              return (
-                <div className='result' key={index}>
-                  <span className='name'>{`Class ${index + 1}`}</span>
-                  <span className='confidence'>Confidence level: {(result * 100).toFixed(2)}%</span>
-                </div>
-              )
-            })}
-          </div>}
+          {results.length > 0 && (
+            <div className="resultsHolder">
+              {results.map((result, index) => {
+                return (
+                  <div className="result" key={index}>
+                    <span className="name">{`Class ${index + 1}`}</span>
+                    <span className="confidence">Confidence level: {(result * 100).toFixed(2)}%</span>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
+        {imageURL && <button className="button" onClick={identify}>Identify Image</button>}
       </div>
-      {history.length > 0 && <div className="recentPredictions">
-        <h2>Recent Images</h2>
-        <div className="recentImages">
-          {history.map((image, index) => {
-            return (
-              <div className="recentPrediction" key={`${image}${index}`}>
-                <img src={image} alt='Recent Prediction' onClick={() => setImageURL(image)} />
-              </div>
-            )
-          })}
+      {history.length > 0 && (
+        <div className="recentPredictions">
+          <h2>Recent Images</h2>
+          <div className="recentImages">
+            {history.map((image, index) => {
+              return (
+                <div className="recentPrediction" key={`${image}${index}`}>
+                  <img src={image} alt="Recent Prediction" onClick={() => setImageURL(image)} />
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
